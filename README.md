fcfc425a6397be4dc9b2aca0bd365d38a9d86748
Jose Lee <jose.lee@basware.com>
2016-10-08 16:42:40 +0300

    Change simple hello world into simple read-only list

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# Hello world
-As simple as it gets
+# List and list item
+Very simple app with two components. Outer `List` component, which renders many child `ListItem` components.
+
+Still read-only, with no interactions, but uses props to pass data downwards.
 
 #### Setup and run
 Run an http-server from the root directory
--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,22 @@
-class HelloWorld extends React.Component {
+class List extends React.Component {
     render() {
+        const myStrings = ['foo', 'bar', 'baz', 'hey', 'wat', 'omg'];
+        const listItems = myStrings.map((myString) => <ListItem text={myString} />);
         return (
-            <div>Hello World! This is react.</div>
+            <ul>
+                {listItems}
+            </ul>
         );
     }
 }
+
+class ListItem extends React.Component {
+    render() {
+        return (
+            <li>{this.props.text}</li>
+        );
+    }
+}
+
 let container = document.querySelector('#app');
-ReactDOM.render(<HelloWorld/>, container);
+ReactDOM.render(<List />, container);

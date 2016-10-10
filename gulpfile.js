93d8d50c1ef19df69de84469a9f28a83a036be2e
Jose Lee <jose.lee@basware.com>
2016-10-10 13:03:35 +0300

    Gulp tasks use fat arrows

--- gulpfile.js
+++ gulpfile.js
@@ -5,30 +5,34 @@ let babelify = require('babelify');
 let source = require('vinyl-source-stream');
 let connect = require('gulp-connect');
 
-gulp.task('copy-index', function () {
+gulp.task('copy-index', () => {
     return gulp.src('./src/index.html')
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('copy-css', function () {
+gulp.task('copy-css', () => {
     return gulp.src('./src/styles/styles.css')
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build-jsx', function () {
-    return browserify({entries: './src/scripts/index.jsx', extensions: ['.jsx'], debug: true})
+gulp.task('build-jsx', () => {
+    return browserify({
+            entries: './src/scripts/index.jsx',
+            extensions: ['.jsx'],
+            debug: true
+        })
         .transform('babelify', {presets: ['es2015', 'react']})
         .bundle()
         .pipe(source('app.js'))
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('watch', ['build-jsx', 'copy-index', 'copy-css'], function () {
+gulp.task('watch', ['build-jsx', 'copy-index', 'copy-css'], () => {
     gulp.watch('./src/**/*.jsx', ['build-jsx']);
     gulp.watch('./src/**/*.css', ['copy-css']);
     gulp.watch('./src/index.html', ['copy-index']);
 });
 
-gulp.task('default', ['watch'], function() {
+gulp.task('default', ['watch'], () => {
     connect.server({root: 'dist', port: 8080});
 });
